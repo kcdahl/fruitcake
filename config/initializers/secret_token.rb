6a6105b5f0bd2da368ef502cd3f18f22d1ae5578
K.C. Dahl <kcdahl2000@gmail.com>
2012-05-02 22:07:50 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fruitcake::Application.config.secret_token = '16a159922f1e463e1680692cbc01e0f7ba8aa457759966cf1fc54e41e4127cd168d5648ca671faf826bda5bbc38b12a4d4d1fa3666c2b81902ee9914daf83727'
