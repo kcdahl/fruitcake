f351e953c1b50ff047cf8b3e38f90698a8c70ae7
K.C. Dahl <kcdahl2000@gmail.com>
2012-05-04 12:09:15 -0700

    updated

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,26 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+describe "Static Pages" do
   describe "Home page" do
-    it "should have the content 'Fruitcake'" do
+    it "should have the content 'xxx'" do
       visit '/static_pages/home'
-      page.should have_content('Fruitcake')
+      page.should have_content('xxx')
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+	visit '/static_pages/help'
+	page.should have_content('Help')
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the content 'About Me'" do
+      visit '/static_pages/about'
+      page.should have_content('About Me')
     end
   end
 end
